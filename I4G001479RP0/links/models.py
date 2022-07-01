a91bbbe6ae84cd75ed847c3fc9150f018d46a1cf
Trish <103179004+trish-m@users.noreply.github.com>
2022-07-01 23:53:48 +0300

    Update models.py

--- I4G001479RP0/links/models.py
+++ I4G001479RP0/links/models.py
@@ -25,9 +25,3 @@ class Link(models.Model):
 
 
 
-# target_url : A url path of maxlength 200, use Django’s models.URLField
-# description : A string of maxlength 200, use Django’s models.CharField
-# identifier: A string of maxlength 20, use Django’s models.SlugField. Set blank=True and unique=True for the field.
-# author : A Foreign Key to the current user model. Make use of Django’s get_user_model function.
-# created_date : A date-time column, use Django’s models.DateTimeField.
-# active :  A boolean (True or False), determining if the shortened URL is publicly accessible. Make use of Django’s BooleanField. The default should be True.
